1c765849df3afc3744dcc9c27571ef33024bbb04
Pedro404r <jessicaulissesalbardeiro@gmail.com>
2025-12-02 18:41:25 -0300

    Alterações na API do fabric, atualizando a versão de 0.16.7 para 0.16.10, além da correção dos erros relacionados aos imports incorretos em todos os arquivos .java

--- fabric-1.21.1/src/main/java/com/whoami/mods/item/ItemsRegister.java
+++ fabric-1.21.1/src/main/java/com/whoami/mods/item/ItemsRegister.java
@@ -1,6 +1,7 @@
 package com.whoami.mods.item;
 
 import com.whoami.mods.Mod;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.SwordItem;
@@ -11,48 +12,32 @@ import net.minecraft.util.Identifier;
 
 public class ItemsRegister {
 
-    public static final Item AETHER_INGOT = register(
-        "aether_ingot", 
-        new Item(new Item.Settings())
-    );
+  public static final Item AETHER_INGOT = register("aether_ingot", new Item(new Item.Settings()));
 
-    public static final Item MONARCH_SWORD = register(
-        "monarch_sword", 
-        new SwordItem(ToolMaterials.DIAMOND, new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.DIAMOND, 3, -2.4F)))
-    );
-    
-    public static final Item REGAL_PICKAXE = register(
-        "regal_pickaxe", 
-        new PickaxeItem(ToolMaterials.DIAMOND, new Item.Settings())
-    );
+  public static final Item MONARCH_SWORD = register("monarch_sword",
+      new SwordItem(ToolMaterials.DIAMOND, new Item.Settings().attributeModifiers(
+          SwordItem.createAttributeModifiers(ToolMaterials.DIAMOND, 3, -2.4F))));
 
-    public static final Item ROYAL_COIN = register(
-    	"royal_coin",
-    	new Item(new Item.Settings())
-    );
-    
-    public static final Item ROYAL_COIN_BLOCK = register(
-    	"royal_coin_block",
-    	new Item(new Item.Settings())
-    );
-    
-    public static final Item ROYAL_FEAST = register(
-        	"royal_feast",
-        	new Item(new Item.Settings()
-                .food(ModFoodComponents.ROYAL_FEAST))
-     );
-    
-    
-    public static final Item SOVEREIGN_COAL = register(
-        	"sovereign_coal",
-        	new Item(new Item.Settings())
-        );
-    
-    private static Item register(String name, Item item) { 
-        return Registry.register(Registries.ITEM, Identifier.of(Mod.MOD_ID, name), item);
-    }
-    
-    public static void registerItems() {
-        Mod.LOGGER.info("Registrando todos os itens do mod");
-    }
+
+  public static final Item REGAL_PICKAXE =
+      register("regal_pickaxe", new PickaxeItem(ToolMaterials.DIAMOND, new Item.Settings()));
+
+  public static final Item ROYAL_COIN = register("royal_coin", new Item(new Item.Settings()));
+
+  public static final Item ROYAL_COIN_BLOCK =
+      register("royal_coin_block", new Item(new Item.Settings()));
+
+  public static final Item ROYAL_FEAST =
+      register("royal_feast", new Item(new Item.Settings().food(ModFoodComponents.ROYAL_FEAST)));
+
+  public static final Item SOVEREIGN_COAL =
+      register("sovereign_coal", new Item(new Item.Settings()));
+
+  private static Item register(String name, Item item) {
+    return Registry.register(Registries.ITEM, Identifier.of(Mod.MOD_ID, name), item);
+  }
+
+  public static void registerItems() {
+    Mod.LOGGER.info("Registrando todos os itens do mod");
+  }
 }
