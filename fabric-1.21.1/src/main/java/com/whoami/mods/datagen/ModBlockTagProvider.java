c41d3919163751e281300f74cdc7e07e1a0533ed
Pedro404r <jessicaulissesalbardeiro@gmail.com>
2025-12-23 14:28:55 -0300

    Melhoria no banner, novas texturas, datagen 2, modtagprovider, refatoração da tab

--- fabric-1.21.1/src/main/java/com/whoami/mods/datagen/ModBlockTagProvider.java
+++ fabric-1.21.1/src/main/java/com/whoami/mods/datagen/ModBlockTagProvider.java
@@ -14,9 +14,29 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
             .add(BlocksRegister.ROYAL_COIN_BLOCK)
-            .add(BlocksRegister.KING_DEEPSLATE_ORE_BLOCK) 
-            .add(BlocksRegister.KING_ORE_BLOCK);
+            .add(BlocksRegister.KING_ORE_BLOCK)
+            .add(BlocksRegister.KING_DEEPSLATE_ORE_BLOCK)
+            .add(BlocksRegister.ROYAL_COIN_STAIRS)
+            .add(BlocksRegister.ROYAL_COIN_SLAB)
+            .add(BlocksRegister.ROYAL_COIN_FENCE)
+            .add(BlocksRegister.ROYAL_COIN_WALL)
+            .add(BlocksRegister.ROYAL_COIN_DOOR)
+            .add(BlocksRegister.ROYAL_COIN_TRAPDOOR)
+            .add(BlocksRegister.ROYAL_COIN_PRESSURE_PLATE)
+            .add(BlocksRegister.ROYAL_COIN_BUTTON);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+            .add(BlocksRegister.KING_ORE_BLOCK)
+            .add(BlocksRegister.KING_DEEPSLATE_ORE_BLOCK);
+
+        getOrCreateTagBuilder(BlockTags.FENCES).add(BlocksRegister.ROYAL_COIN_FENCE);
+        getOrCreateTagBuilder(BlockTags.WALLS).add(BlocksRegister.ROYAL_COIN_WALL);
+        getOrCreateTagBuilder(BlockTags.DOORS).add(BlocksRegister.ROYAL_COIN_DOOR);
+        getOrCreateTagBuilder(BlockTags.TRAPDOORS).add(BlocksRegister.ROYAL_COIN_TRAPDOOR);
+        getOrCreateTagBuilder(BlockTags.PRESSURE_PLATES).add(BlocksRegister.ROYAL_COIN_PRESSURE_PLATE);
+        getOrCreateTagBuilder(BlockTags.BUTTONS).add(BlocksRegister.ROYAL_COIN_BUTTON);
     }
 }
--- fabric-1.21.1/src/main/java/com/whoami/mods/datagen/ModLootTableProvider.java
+++ fabric-1.21.1/src/main/java/com/whoami/mods/datagen/ModLootTableProvider.java
@@ -1,5 +1,54 @@
 package com.whoami.mods.datagen;
 
-public class ModLootTableProvider {
+import com.whoami.mods.block.BlocksRegister;
+import com.whoami.mods.item.ItemsRegister;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
 
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    public ModLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(BlocksRegister.ROYAL_COIN_BLOCK);
+        addDrop(BlocksRegister.KING_ORE_BLOCK);
+        addDrop(BlocksRegister.ROYAL_COIN_STAIRS);
+        addDrop(BlocksRegister.ROYAL_COIN_FENCE);
+        addDrop(BlocksRegister.ROYAL_COIN_WALL);
+        addDrop(BlocksRegister.ROYAL_COIN_PRESSURE_PLATE);
+        addDrop(BlocksRegister.ROYAL_COIN_BUTTON);
+        addDrop(BlocksRegister.ROYAL_COIN_TRAPDOOR);
+
+        addDrop(BlocksRegister.ROYAL_COIN_SLAB, slabDrops(BlocksRegister.ROYAL_COIN_SLAB));
+        addDrop(BlocksRegister.ROYAL_COIN_DOOR, doorDrops(BlocksRegister.ROYAL_COIN_DOOR));
+
+        addDrop(BlocksRegister.KING_DEEPSLATE_ORE_BLOCK, 
+            multipleOreDrops(BlocksRegister.KING_DEEPSLATE_ORE_BLOCK, ItemsRegister.ROYAL_COIN, 2.0f, 5.0f));
+    }
+
+    public LootTable.Builder multipleOreDrops(Block drop, Item item, float min, float max) {
+        RegistryWrapper.Impl<Enchantment> enchantmentLookup = registryLookup.getWrapperOrThrow(RegistryKeys.ENCHANTMENT);
+        return dropsWithSilkTouch(drop, 
+            (LootPoolEntry.Builder<?>)this.applyExplosionDecay(drop, 
+                ((LeafEntry.Builder<?>)ItemEntry.builder(item)
+                    .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(min, max))))
+                    .apply(ApplyBonusLootFunction.oreDrops(enchantmentLookup.getOrThrow(Enchantments.FORTUNE)))));
+    }
 }
